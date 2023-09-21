c7e597855db2b8edff2f95bc42825a30e04b2768
juancamilogarcue <juankgarcue@gmail.com>
2023-09-21 12:18:05 -0500

    Listo menú de escritorio y movil

--- mian.js
+++ mian.js
@@ -1,13 +1,20 @@
 const menuEmail = document.querySelector(".navbar-email");
 const desktopMenu = document.querySelector(".desktop-menu");
+const hamburgesaMenu = document.querySelector(".menu");
+const mobilMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", aparecerMenu);
+hamburgesaMenu.addEventListener("click", aparecerMenuHamburgesa);
 
 function aparecerMenu () {
     console.log("click")
     desktopMenu.classList.toggle("inactive");
 }
 
-console.log("JS FUNCIONANDO");
+function aparecerMenuHamburgesa () {
+    console.log("click")
+    mobilMenu.classList.toggle("inactive");
+}
+
 
 
